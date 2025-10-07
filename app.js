c1c1f409db46635e0022fe01b4f21ef85bf4a9d6
Mehtab Ahmed <mehtabahmed7777777@gmail.com>
2025-10-07 14:45:06 +0500

    cors issue fixed

--- app.js
+++ app.js
@@ -118,11 +118,7 @@ app.set("view engine", "jade");
 // Middleware setup
 app.use(
   cors({
-    origin: [
-      "http://localhost:3000",
-      "http://localhost:5173",
-      "http://127.0.0.1:5173",
-    ],
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173", "13.233.6.224"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
