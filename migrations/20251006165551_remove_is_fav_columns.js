aeedcd95a3a05a05a71a6aa997256bfbf62ab819
haiderarshad021 <haiderarshad021@gmail.com>
2025-10-07 17:24:02 +0500

    fix rendering & normal user ui issues

--- migrations/20251006165551_remove_is_fav_columns.js
+++ migrations/20251006165551_remove_is_fav_columns.js
@@ -2,28 +2,40 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  return Promise.all([
-    knex.schema.alterTable('user_favourite_folders', function(table) {
-      table.dropColumn('is_fav');
-    }),
-    knex.schema.alterTable('user_favourite_files', function(table) {
-      table.dropColumn('is_fav');
-    })
-  ]);
+exports.up = async function (knex) {
+  const hasFavFolders = await knex.schema.hasColumn("user_favourite_folders", "is_favourite");
+  const hasFavFiles = await knex.schema.hasColumn("user_favourite_files", "is_favourite");
+
+  if (hasFavFolders) {
+    await knex.schema.alterTable("user_favourite_folders", function (table) {
+      table.dropColumn("is_favourite");
+    });
+  }
+
+  if (hasFavFiles) {
+    await knex.schema.alterTable("user_favourite_files", function (table) {
+      table.dropColumn("is_favourite");
+    });
+  }
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  return Promise.all([
-    knex.schema.alterTable('user_favourite_folders', function(table) {
-      table.integer('is_fav').defaultTo(1);
-    }),
-    knex.schema.alterTable('user_favourite_files', function(table) {
-      table.integer('is_fav').defaultTo(1);
-    })
-  ]);
+exports.down = async function (knex) {
+  const hasFavFolders = await knex.schema.hasColumn("user_favourite_folders", "is_favourite");
+  const hasFavFiles = await knex.schema.hasColumn("user_favourite_files", "is_favourite");
+
+  if (!hasFavFolders) {
+    await knex.schema.alterTable("user_favourite_folders", function (table) {
+      table.integer("is_favourite").defaultTo(0);
+    });
+  }
+
+  if (!hasFavFiles) {
+    await knex.schema.alterTable("user_favourite_files", function (table) {
+      table.integer("is_favourite").defaultTo(0);
+    });
+  }
 };
